49c45196d8bb8cb4bab9b88b6547cf069242248d
oxxy <53953876+brunovais@users.noreply.github.com>
2024-07-29 08:58:12 -0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@ apktool b split_config.arm64_v8a -o fucked.apk
 zipalign -p -f 4 fucked.apk split_config.arm64_v8a.apk
 apksigner sign --ks fuck.keystore --ks-pass file:my-passfile.txt --v1-signing-enabled true --v2-signing-enabled true split_config.arm64_v8a.apk
 adb install base.apk
-adb install-multiple -p br.com.rodrigokolb.realdrum split_config.arm64_v8a.apk split_config.xxhdpi.apk split_config.pt.apk
+adb install-multiple -p com.github.4l2f split_config.arm64_v8a.apk split_config.xxhdpi.apk split_config.pt.apk
